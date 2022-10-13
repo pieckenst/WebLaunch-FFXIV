9d6d8045358f4c3f1f1c96b000966645bd8ac41e
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2022-10-14 01:07:48 +0300

    Update Program.cs

--- LaunchApp/Program.cs
+++ LaunchApp/Program.cs
@@ -8,4 +8,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddMasaBlazor(builder =>
+{
+    builder.ConfigureTheme(theme =>
+    {
+        theme.Themes.Light.Primary = "#4318FF";
+        theme.Themes.Light.Accent = "#4318FF";
+    });
+})
 await builder.Build().RunAsync();
