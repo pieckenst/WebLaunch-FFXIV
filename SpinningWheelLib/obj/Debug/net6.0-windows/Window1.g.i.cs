4363a4a98495da86dc3d35e3a94e9b614bf9d495
andrey savich <Yvazhaikorova@outlook.com>
2022-10-13 16:53:04 +0300

    fix some bugs

--- SpinningWheelLib/obj/Debug/net6.0-windows/Window1.g.i.cs
+++ SpinningWheelLib/obj/Debug/net6.0-windows/Window1.g.i.cs
@@ -54,7 +54,7 @@ namespace SpinningWheelLib {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SpinningWheelLib;V1.0.0.0;component/window1.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SpinningWheelLib;component/window1.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Window1.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
