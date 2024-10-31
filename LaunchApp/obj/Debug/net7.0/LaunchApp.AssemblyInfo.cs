aa3e56385070aa9b5f7e13b3242a039462424f7c
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2024-10-31 20:02:26 +0300

    ED

--- LaunchApp/obj/Debug/net7.0/LaunchApp.AssemblyInfo.cs
+++ LaunchApp/obj/Debug/net7.0/LaunchApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaunchApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f10a229b08dd75222fe74f8f48633e19cf068ec6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68f0bfe0446b573eed985bbe0583b06db763ab4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaunchApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaunchApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
