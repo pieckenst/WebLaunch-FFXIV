774fda3f80e26fa3b842e95d9ee59ec7ce5e942e
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2024-11-24 00:31:57 +0300

    edits and WIP SLATE THEME

--- SpinningWheelLib/CustomLonghornTitleBar.xaml.cs
+++ SpinningWheelLib/CustomLonghornTitleBar.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace SpinningWheelLib.Controls
 {
@@ -16,6 +17,40 @@ namespace SpinningWheelLib.Controls
 
         public static readonly DependencyProperty AdditionalContentProperty =
             DependencyProperty.Register("AdditionalContent", typeof(object), typeof(CustomLonghornTitleBar), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty UseSlateThemeProperty =
+        DependencyProperty.Register("UseSlateTheme", typeof(bool), typeof(CustomLonghornTitleBar),
+            new PropertyMetadata(true, OnUseSlateThemeChanged));
+
+
+
+        public bool UseSlateTheme
+        {
+            get { return (bool)GetValue(UseSlateThemeProperty); }
+            set { SetValue(UseSlateThemeProperty, value); }
+        }
+
+        private static void OnUseSlateThemeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as CustomLonghornTitleBar;
+            if (control != null)
+            {
+                control.UpdateBackground();
+            }
+        }
+
+        private void UpdateBackground()
+        {
+            if (UseSlateTheme)
+            {
+                glassArea.Background = (LinearGradientBrush)FindResource("TaskbarBackground");
+            }
+            else
+            {
+                // Use Aero/Window background
+                glassArea.Background = Background;
+            }
+        }
 
         public object TitleBarContent
         {
@@ -42,28 +77,29 @@ namespace SpinningWheelLib.Controls
         }
 
         public CustomLonghornTitleBar()
-{
-    InitializeComponent();
-    
-    MinButton.Click += (s, e) => { 
-        var window = Window.GetWindow(this);
-        if (window != null) window.WindowState = WindowState.Minimized;
-    };
-    
-    MaxButton.Click += (s, e) => {
-        var window = Window.GetWindow(this);
-        if (window != null) {
-            window.WindowState = window.WindowState == WindowState.Maximized 
-                ? WindowState.Normal 
-                : WindowState.Maximized;
+        {
+            InitializeComponent();
+
+            MinButton.Click += (s, e) => {
+                var window = Window.GetWindow(this);
+                if (window != null) window.WindowState = WindowState.Minimized;
+            };
+
+            MaxButton.Click += (s, e) => {
+                var window = Window.GetWindow(this);
+                if (window != null)
+                {
+                    window.WindowState = window.WindowState == WindowState.Maximized
+                        ? WindowState.Normal
+                        : WindowState.Maximized;
+                }
+            };
+
+            CloseButton.Click += (s, e) => {
+                var window = Window.GetWindow(this);
+                if (window != null) window.Close();
+            };
         }
-    };
-    
-    CloseButton.Click += (s, e) => {
-        var window = Window.GetWindow(this);
-        if (window != null) window.Close();
-    };
-}
 
 
         public void ShowFavoriteButton(bool show)
@@ -77,21 +113,21 @@ namespace SpinningWheelLib.Controls
         }
 
         public event RoutedEventHandler FavoriteClick
-{
-    add { favButton.Click += value; }
-    remove { favButton.Click -= value; }
-}
+        {
+            add { favButton.Click += value; }
+            remove { favButton.Click -= value; }
+        }
 
-public event RoutedEventHandler BackClick
-{
-    add { backButton.Click += value; }
-    remove { backButton.Click -= value; }
-}
+        public event RoutedEventHandler BackClick
+        {
+            add { backButton.Click += value; }
+            remove { backButton.Click -= value; }
+        }
 
-public event RoutedEventHandler ForwardClick
-{
-    add { forwardButton.Click += value; }
-    remove { forwardButton.Click -= value; }
-}
+        public event RoutedEventHandler ForwardClick
+        {
+            add { forwardButton.Click += value; }
+            remove { forwardButton.Click -= value; }
+        }
     }
 }
